f3d3f70f46b9784278d973306012c511068429b2
yan yibin <bassamyan@163.com>
2015-01-23 14:15:42 +0800

    V2.0

--- BYDailyNews/BYMainController.m
+++ BYDailyNews/BYMainController.m
@@ -13,9 +13,7 @@
 #import "BYSelectNewBar.h"
 
 @interface BYMainController ()
-{
-    CGFloat nav_height;
-}
+
 @end
 
 @implementation BYMainController
@@ -24,29 +22,23 @@
     [super viewDidLoad];
 
     [self setupNaviBar];
+    
     [self makeContent];
-
 }
 
 -(void)setupNaviBar
 {
-    nav_height = self.navigationController.navigationBar.frame.size.height + [UIApplication sharedApplication].statusBarFrame.size.height;
-    [UIApplication sharedApplication].statusBarHidden = YES;
-    self.view.backgroundColor = Color_main;
+    self.view.backgroundColor = [UIColor whiteColor];
     [self.navigationController.navigationBar setTranslucent:NO];
-    self.navigationController.navigationBar.hidden = YES;
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-//    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"nav_bg"] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"nav_bg"] forBarMetrics:UIBarMetricsDefault];
     
-    UIImageView *background = [[UIImageView alloc] initWithFrame:CGRectMake(0, conditionScrollH+nav_height, BYScreenWidth, BYScreenHeight-conditionScrollH)];
-    background.image = [UIImage imageNamed:@"content_bg.png"];
-    [self.view addSubview:background];
 }
 
 
 -(void)makeContent
 {
-    BYConditionBar *conditionBar = [[BYConditionBar alloc] initWithFrame:CGRectMake(0, nav_height, BYScreenWidth, conditionScrollH)];
+    BYConditionBar *conditionBar = [[BYConditionBar alloc] initWithFrame:CGRectMake(0, 0, BYScreenWidth, conditionScrollH)];
     [self.view addSubview:conditionBar];
     
     
@@ -54,17 +46,13 @@
     [self.view addSubview:selection_newBar];
     
     
-    BYSelectionDetails *selection_details = [[BYSelectionDetails alloc] initWithFrame:CGRectMake(0, nav_height+conditionScrollH-BYScreenHeight, BYScreenWidth, BYScreenHeight-conditionScrollH)];
+    BYSelectionDetails *selection_details = [[BYSelectionDetails alloc] initWithFrame:CGRectMake(0, conditionScrollH-BYScreenHeight, BYScreenWidth, BYScreenHeight-conditionScrollH)];
     [self.view insertSubview:selection_details belowSubview:conditionBar];
     
     
-    SelectionButton *arrow = [[SelectionButton alloc] initWithFrame:CGRectMake(BYScreenWidth-arrow_width, nav_height, arrow_width, conditionScrollH)];
+    SelectionButton *arrow = [[SelectionButton alloc] initWithFrame:CGRectMake(BYScreenWidth-arrow_width, 0, arrow_width, conditionScrollH)];
     arrow -> NewBar = selection_newBar;
     arrow -> Details = selection_details;
     [self.view addSubview:arrow];
-    
-    UIImageView *custom = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, BYScreenWidth, nav_height)];
-    custom.image = [UIImage imageNamed:@"nav_background2.png"];
-    [self.view insertSubview:custom atIndex:100];
 }
 @end
